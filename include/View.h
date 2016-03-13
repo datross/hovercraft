772425141135457992c47214fff538212823829d
yoanlcq <yoanlecoq.io@gmail.com>
2016-03-13 19:19:40 +0100

    View: Fini mapCoordsToPixels().

--- include/View.h
+++ include/View.h
@@ -7,17 +7,16 @@ typedef struct {
     float left, right, bottom, top;
 } View;
 
+/* Ces fonctions ont été testées que sur le papier. */
 static inline void View_mapPixelToCoords(Vec2 *out, const Vec2u *coord, 
                            const Vec2u *win_size, const View *view) {
     out->x = view->left + (view->right - view->left)*coord->x/(float)win_size->x;
     out->y = view->top  - (view->top - view->bottom)*coord->y/(float)win_size->y;
 }
-/* TODO Test me ! */
 static inline void View_mapCoordToPixels(Vec2u *out, const Vec2u *coord, 
                           const Vec2u *win_size, const View *view) {
     out->x = win_size->x*(coord->x-view->left)/(view->right - view->left);
-    // Celle-là est bogus. FIXME
-    //out->y = -win_size->y*(coord->y-view->bottom)/(view->top - view->bottom);
+    out->y = win_size->y*(1.f - (coord->y + view->bottom)/(view->top - view->bottom));
 }
 
 #endif /* VIEW_H */
