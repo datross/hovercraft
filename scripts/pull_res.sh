9a35b0490d4def593b7230d42a4b7ebacf57bfad
yoanlcq <yoanlecoq.io@gmail.com>
2016-04-14 14:05:50 +0200

    Structures de menus.

--- scripts/pull_res.sh
+++ scripts/pull_res.sh
@@ -8,10 +8,22 @@ user hovercraft
 passive
 prompt
 binary
+newer music/otherworld.flac
 newer ships/dolphin_unicorn.txt
 newer ships/dolphin_unicorn.png
 newer ships/dolphin_unicorn.pal.png
+newer ships/raging_waffle.txt
+newer ships/raging_waffle.png
+newer ships/raging_waffle.pal.png
+newer ships/darkness_of_evil.txt
+newer ships/darkness_of_evil.png
+newer ships/darkness_of_evil.pal.png
+newer ships/completely_regular_ship.txt
+newer ships/completely_regular_ship.png
+newer ships/completely_regular_ship.pal.png
 newer maps/bigblue.txt
 newer maps/bigblue.png
+newer maps/dawn.txt
+newer maps/dawn.png
 bye
 EOF
--- scripts/push_res.sh
+++ scripts/push_res.sh
@@ -7,10 +7,22 @@ user hovercraft
 passive
 prompt
 binary
+put music/otherworld.flac
 put ships/dolphin_unicorn.txt
 put ships/dolphin_unicorn.png
 put ships/dolphin_unicorn.pal.png
+put ships/raging_waffle.txt
+put ships/raging_waffle.png
+put ships/raging_waffle.pal.png
+put ships/darkness_of_evil.txt
+put ships/darkness_of_evil.png
+put ships/darkness_of_evil.pal.png
+put ships/completely_regular_ship.txt
+put ships/completely_regular_ship.png
+put ships/completely_regular_ship.pal.png
 put maps/bigblue.txt
 put maps/bigblue.png
+put maps/dawn.txt
+put maps/dawn.png
 bye
 EOF
