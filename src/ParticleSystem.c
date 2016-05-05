eb6605c4958b83ecb4bb2b0505f7f745cda48649
datross <datross@openmailbox.org>
2016-05-05 23:06:47 +0200

    pb gl_Blend

--- src/ParticleSystem.c
+++ src/ParticleSystem.c
@@ -37,7 +37,6 @@ void ParticleSystem_compute_step(const ParticleSystem * ps, float elapsed_time) 
 }
 
 void ParticleSystem_draw_particles(const ParticleSystem * ps) {
-    glEnable(GL_BLEND);
     glEnable(GL_POINT_SMOOTH); /* TODO : Ne marche pas */
     GLfloat col[4];
     glGetFloatv(GL_CURRENT_COLOR, col);
@@ -51,7 +50,6 @@ void ParticleSystem_draw_particles(const ParticleSystem * ps) {
         glEnd();
     }
     glColor4f(col[0], col[1], col[2], col[3]);
-    glDisable(GL_BLEND);
 }
         
 void Particle_init(Particle * p, const ParticleSystem * ps) {
