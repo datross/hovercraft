5833cdd4cf3b166db3d91b7f35924458ee6eb726
yoanlcq <yoanlecoq.io@gmail.com>
2016-05-08 21:44:59 +0200

    Résolu le problème de la barre noire au dessus du 'o' Monospace.

--- scripts/push_res.sh
+++ scripts/push_res.sh
@@ -8,29 +8,34 @@ user hovercraft
 passive
 prompt
 binary
-put menus/menus.txt
-put menus/menus.png
-put menus/main_menu_music.mp3
 put ships/dolphin_unicorn.txt
-put ships/dolphin_unicorn.png
-put ships/dolphin_unicorn.pal.png
+put ships/dolphin_unicorn_pal.png
+put ships/dolphin_unicorn_tex.png
+put ships/dolphin_unicorn_artwork.png
 put ships/raging_waffle.txt
-put ships/raging_waffle.png
-put ships/raging_waffle.pal.png
-put ships/darkness_of_evil.txt
-put ships/darkness_of_evil.png
-put ships/darkness_of_evil.pal.png
+put ships/raging_waffle_pal.png
+put ships/raging_waffle_tex.png
+put ships/raging_waffle_artwork.png
 put ships/regular_ship.txt
-put ships/regular_ship.png
-put ships/regular_ship.pal.png
+put ships/regular_ship_pal.png
+put ships/regular_ship_tex.png
+put ships/regular_ship_artwork.png
+put ships/darkness_of_evil.txt
+put ships/darkness_of_evil_pal.png
+put ships/darkness_of_evil_tex.png
+put ships/darkness_of_evil_artwork.png
 put maps/bigblue.txt
 put maps/bigblue.png
 put maps/bigblue_tile.png
-put maps/dawn.txt
-put maps/dawn.png
-put maps/dawn_tile.png
-put maps/pensif_race.mp3
+put maps/zero.txt
+put maps/zero.png
+put maps/zero_tile.png
+put maps/slide_city.txt
+put maps/slide_city.png
+put maps/slide_city_tile.png
+put maps/inferno.txt
+put maps/inferno.png
+put maps/inferno_tile.png
 put snd/menu_transition.wav
-put snd/clap_close.wav
 bye
 EOF
